bec8e7fb6a2a6d520ced99f534504ea8e49836bc
ThaynaraSP <thaynarasilvapinto@hotmail.com>
2017-06-29 17:05:09 -0300

    Update P010 - rotacao90.c

--- P010 - rotacao90.c
+++ P010 - rotacao90.c
@@ -2,12 +2,12 @@
 #include<stdio.h>
 
 int main(int argc, char *argv[]){
-    int n=0,i=0,j=0,aux=0,k=0,K=0;
+    int n=0,i=0,j=0,aux=0,k=0,K=0, aux1 = 0;
 
     printf("Digite um valor inteiro (n) que define a dimensao da matriz nxn: \n");
     scanf("%d",&n);
 
-    int M[n][n], B[n][n];
+    int M[n][n], a=0,b=0,c=0,d=0; //B[n][n];
     ///Ler a matriz
 
     while(i<n){
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]){
         i++;
     }
 
-    ///Código que gira a matriz
+    ///CÃ³digo que gira a matriz
 
     i=0;
     j=n-1;
@@ -28,41 +28,13 @@ int main(int argc, char *argv[]){
         j=n-1;
         K=0;
         while(j>=0){
-             B[k][K] = M[j][i];
+             printf("%d ",M[j][i]);
              j--;
-             K++;
+
         }
+        putchar('\n');
         k++;
         i++;
     }
     putchar('\n');
-    ///Imprimir a inicial
-    printf("A matriz digitada \n");
-    i=0;
-    j=0;
-    while(i<n){
-        j=0;
-        while(j<n){
-            printf("%d ",M[i][j]);
-            j++;
-        }
-        printf("\n");
-        i++;
-    }
-    putchar('\n');
-    ///Imprimir a matriz final
-    printf("A matriz rodada 90 graus \n");
-    i=0;
-    j=0;
-    while(i<n){
-        j=0;
-        while(j<n){
-            printf("%d ",B[i][j]);
-            j++;
-        }
-        printf("\n");
-        i++;
-    }
-
-
     return 0;}
